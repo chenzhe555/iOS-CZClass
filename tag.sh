372908743d639ac2cc9fd97941a944ed354241bb
chenzhe <chenzhe@meicai.cn>
2019-04-29 22:42:30 +0800

    replace tag

--- tag.sh
+++ tag.sh
@@ -1,1 +1,2 @@
-git add . && git commit -m 'replace tag' && git push origin && git tag -d $1 && git push origin :refs/tags/`$1` && git tag $1 && git push --tags
+path=":refs/tags/"$1
+git add . && git commit -m 'replace tag' && git push origin && git tag -d $1 && git push origin $path && git tag $1 && git push --tags
